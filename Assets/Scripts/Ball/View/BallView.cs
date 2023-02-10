3859fc575c45257acd3a314c658b02c3dbc2f0ce
billsmoke <dumanbs@gmail.com>
2023-02-10 13:28:52 +0300

    placing indicator finished

--- Assets/Scripts/Ball/View/BallView.cs
+++ Assets/Scripts/Ball/View/BallView.cs
@@ -13,6 +13,7 @@ namespace Ball.View
         private IGameRepository _gameRepository;
             
         [SerializeField] private Rigidbody _rigidBody;
+        private GameObject _placingIndicators;
         private Vector3 _startPos;
         private Vector3 _offset;
         
@@ -23,12 +24,17 @@ namespace Ball.View
             _camera = Camera.main;
             _gameView = gameView;
             _gameRepository = gameRepository;
+            _placingIndicators = GameObject.FindWithTag("PlacingIndicators");
         }
 
         private void OnMouseDown()
         {
             Vector3 cameraVector = _camera.ScreenToWorldPoint(Input.mousePosition);
             _startPos = transform.position;
+            if (_gameRepository.IsMovable)
+            {
+                ShowPlacingIndicators();
+            }
             _offset = new Vector3(cameraVector.x,.5f,cameraVector.z) - _startPos;
             _rigidBody.isKinematic = false;
         }
@@ -46,6 +52,7 @@ namespace Ball.View
 
         private void OnMouseUp()
         {
+            HidePlacingIndicators();
             _rigidBody.isKinematic = true;
             _rigidBody.velocity = Vector3.zero;
             Vector3 currentPos = transform.position;
@@ -73,6 +80,50 @@ namespace Ball.View
             Vector3 cameraVector = _camera.ScreenToWorldPoint(Input.mousePosition);
             Vector3 mousePos = new Vector3(cameraVector.x,.5f,cameraVector.z);
             return mousePos;
+        }
+
+        private void ShowPlacingIndicators()
+        {
+            float maxPosition = Mathf.Sqrt(_gameRepository.BoardSize) / 2;
+            if (_placingIndicators != null)
+            {
+                GameObject right = _placingIndicators.transform.GetChild(0).gameObject;
+                GameObject left = _placingIndicators.transform.GetChild(1).gameObject;
+                GameObject up = _placingIndicators.transform.GetChild(2).gameObject;
+                GameObject down = _placingIndicators.transform.GetChild(3).gameObject;
+                
+                if (_startPos.x+1 < maxPosition)
+                {
+                    right.transform.position = new Vector3(_startPos.x+1,-.4f,_startPos.z);
+                    right.SetActive(true);
+                }
+                if (_startPos.x-1 > -maxPosition)
+                {
+                    left.transform.position = new Vector3(_startPos.x-1,-.4f,_startPos.z);
+                    left.SetActive(true);
+                }
+                if (_startPos.z+1 < maxPosition)
+                {
+                    up.transform.position = new Vector3(_startPos.x,-.4f,_startPos.z+1);
+                    up.SetActive(true);
+                }
+                if (_startPos.z-1 > -maxPosition)
+                {
+                    down.transform.position = new Vector3(_startPos.x,-.4f,_startPos.z-1);
+                    down.SetActive(true);
+                }
+            }
+        }
+
+        private void HidePlacingIndicators()
+        {
+            if (_placingIndicators != null)
+            {
+                _placingIndicators.transform.GetChild(0).gameObject.SetActive(false);
+                _placingIndicators.transform.GetChild(1).gameObject.SetActive(false);
+                _placingIndicators.transform.GetChild(2).gameObject.SetActive(false);
+                _placingIndicators.transform.GetChild(3).gameObject.SetActive(false);
+            }
         }
 
         public class Factory : PlaceholderFactory<BallView>
